702d6d17b4c4a5800ec7fccacc5fa071fca78404
David Connell <davecon88@gmail.com>
2020-09-12 11:19:51 +0930

    initialisation of files

---
+++ Unsolved/script.js
@@ -0,0 +1,18 @@
+<script type="text/javascript">
+
+$(document).ready(function() {
+$('').on('click'
+$('button').on('click'
+$('button').on('click'
+$('button').on('click'
+$('button').on('click'
+$('button').on('click'
+$('button').on('click'
+$('button').on('click'
+
+// Your code here...
+
+
+});
+
+</script>
